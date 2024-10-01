7a1a5bc46379eb3f545c35381b565f609991c1ad
MPapyrus <secenmasterdead@yandex.ru>
2024-10-01 22:41:54 +0500

    Project ShoppingList

---
+++ app/src/main/java/com/example/shoppinglist/domain/ShopItem.kt
@@ -0,0 +1,12 @@
+package com.example.shoppinglist.domain
+
+data class ShopItem(
+    val name: String,
+    val count: Int,
+    val enabled: Boolean,
+    var id: Int = -1
+){
+    companion object{
+        const val UNDEFINED_ID = -1
+    }
+}
