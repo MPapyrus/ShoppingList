2979987421bcd821980191fc5ab9685a60e23d8c
MPapyrus <secenmasterdead@yandex.ru>
2025-05-20 21:21:50 +0500

    Request data from db

--- app/src/main/java/com/example/shoppinglist/data/ShopListProvider.kt
+++ app/src/main/java/com/example/shoppinglist/data/ShopListProvider.kt
@@ -6,14 +6,27 @@ import android.content.UriMatcher
 import android.database.Cursor
 import android.net.Uri
 import android.util.Log
+import com.example.shoppinglist.di.DaggerApplicationComponent
+import com.example.shoppinglist.presentation.ShopListApp
+import javax.inject.Inject
 
 class ShopListProvider: ContentProvider() {
 
+    private val component by lazy {
+        (context as ShopListApp).component
+    }
+
+    @Inject
+    lateinit var shopListDao: ShopListDao
+
     private val uriMatcher = UriMatcher(UriMatcher.NO_MATCH).apply {
         addURI("com.example.shoppinglist", "shop_items", GET_SHOP_ITEMS_QUERY)
+        addURI("com.example.shoppinglist", "shop_items/#", GET_SHOP_ITEM_BY_ID_QUERY)
+
     }
 
     override fun onCreate(): Boolean {
+        component.inject(this)
         return true
     }
 
@@ -26,14 +39,16 @@ class ShopListProvider: ContentProvider() {
     ): Cursor? {
         val code = uriMatcher.match(p0)
 
-        when(code){
+        return when(code){
             GET_SHOP_ITEMS_QUERY -> {
-
+                shopListDao.getShopListCursor()
+            }
+            else -> {
+                null
             }
         }
 
         Log.d("ShopListProvider", "query uri: $p0 code: $code")
-        return null
     }
 
     override fun getType(p0: Uri): String? {
@@ -55,6 +70,7 @@ class ShopListProvider: ContentProvider() {
     companion object {
 
         const val GET_SHOP_ITEMS_QUERY = 100
+        const val GET_SHOP_ITEM_BY_ID_QUERY = 101
     }
 
 
