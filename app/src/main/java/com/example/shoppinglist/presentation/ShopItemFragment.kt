d48f58d6c927708f1926e937b2c8484b742f7e40
MPapyrus <secenmasterdead@yandex.ru>
2025-05-22 22:54:10 +0500

    Insert data to DB by Content Provider

--- app/src/main/java/com/example/shoppinglist/presentation/ShopItemFragment.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopItemFragment.kt
@@ -1,6 +1,8 @@
 package com.example.shoppinglist.presentation
 
+import android.content.ContentValues
 import android.content.Context
+import android.net.Uri
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -15,6 +17,7 @@ import com.example.shoppinglist.databinding.FragmentShopItemBinding
 import com.example.shoppinglist.di.DaggerApplicationComponent
 import com.example.shoppinglist.domain.ShopItem
 import javax.inject.Inject
+import kotlin.concurrent.thread
 
 class ShopItemFragment: Fragment() {
 
@@ -162,7 +165,18 @@ class ShopItemFragment: Fragment() {
 
     private fun launchAddMode() {
         binding.saveButton.setOnClickListener {
-            viewModel.addShopItem(binding.etName.text?.toString(), binding.etCount.text?.toString())
+//            viewModel.addShopItem(binding.etName.text?.toString(), binding.etCount.text?.toString())
+            thread {
+                context?.contentResolver?.insert(
+                    Uri.parse("content://com.example.shoppinglist/shop_items"),
+                    ContentValues().apply {
+                        put("id",0)
+                        put("name", binding.etName.text?.toString())
+                        put("count", binding.etCount.text?.toString()?.toInt())
+                        put("enabled", true)
+                    }
+                )
+            }
         }
     }
 
